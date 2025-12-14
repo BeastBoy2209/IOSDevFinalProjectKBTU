be97082f4beb7e7a4763207db07d5dd11ec01e9a
BeastBoy <58637567+BeastBoy2209@users.noreply.github.com>
2025-12-14 20:07:15 +0500

    +template

---
+++ Casino/Casino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Casino
+//
+//  Created by Tim Kharitonov on 14.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
